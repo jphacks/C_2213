94a9cf0def8aa636f329d11b5985aa21f705f3c3
sayyyyyy <d958956a6b650@gmail.com>
2022-10-15 12:07:12 +0900

    サインイン、サインアップに条件追加

--- youlink/src/views/SignIn.vue
+++ youlink/src/views/SignIn.vue
@@ -1,35 +1,51 @@
-<template>
-    <div>
-        <input type="text" v-model="user_email">
-        <input type="text" v-model="user_password">
-        <p>{{error_message}}</p>
-        <button @click="userSignIn(user_email, user_password)">サインイン</button>
-        <button @click="googleSignIn">google登録</button>
-    </div>
-</template>
-
 <script setup lang="ts">
-import { googleSignIn, signIn } from '../stores/auth'
+import { googleSignIn } from '../stores/auth'
 import { ref } from 'vue'
+import { auth } from "../FirebaseConfig"
+import { signInWithEmailAndPassword } from "firebase/auth"
 
 let user_email = ref('')
 let user_password = ref('')
 let error_message = ref('')
 
-const userSignIn = async (user_email: string, user_password: string) => {
-    console.log(user_email)
-    console.log(user_password)
+
+const userSignIn = (user_email: string, user_password: string) => {
     if (!user_email) {
-        error_message = 'メールアドレスを入力してください'
+        error_message.value = 'メールアドレスを入力してください'
         return
     }
-        
     if (!user_password) {
-        error_message = 'パスワードを入力してください'
+        error_message.value = 'パスワードを入力してください'
         return
     }
-     
-    signIn(user_email, user_password)
+
+    signInWithEmailAndPassword(auth, user_email, user_password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+    })
+    .catch((error) => {
+        const error_code = error.code
+        if (error_code === 'auth/invalid-email'){
+            error_message.value = '無効なメールアドレスです'
+        } 
+        else if (error_code === 'auth/wrong-password') {
+            error_message.value = 'パスワードが間違っています'
+        }
+        else {
+            console.log(error.code)
+            error_message.value = 'サインインに失敗しました'
+        }
+    })
 }
 
 </script>
+
+<template>
+    <div>
+        <input type="email" v-model="user_email">
+        <input type="text" v-model="user_password">
+        <p>{{ error_message }}</p>
+        <button @click="userSignIn(user_email, user_password)">サインイン</button>
+        <button @click="googleSignIn">google登録</button>
+    </div>
+</template>
--- youlink/src/views/SignUp.vue
+++ youlink/src/views/SignUp.vue
@@ -1,20 +1,57 @@
 <script setup lang="ts">
-import { googleSignIn, signUp } from '../stores/auth'
+import { googleSignIn } from '../stores/auth'
 import { ref } from 'vue'
+import { auth } from "../FirebaseConfig"
+import { createUserWithEmailAndPassword } from "firebase/auth"
 
 let user_email = ref('')
 let user_password = ref('')
 let user_repassword = ref('')
+let error_message = ref('')
+
 const userSignUp = async (user_email: string, user_password: string) => {
-    signUp(user_email, user_password)
+    if (!user_email) {
+        error_message.value = 'メールアドレスを入力してください'
+        return
+    }
+    if (!user_password) {
+        error_message.value = 'パスワードを入力してください'
+        return
+    }
+    if (!user_repassword) {
+        error_message.value = 'パスワードを再入力してください'
+        return
+    }
+
+    createUserWithEmailAndPassword(auth, user_email, user_password)
+    .then((userCredential) => {
+        console.log("signup")
+        const user = userCredential.user;
+    })
+    .catch((error) => {
+        const error_code = error.code;
+        console.log(error_code)
+
+        if (error_code === 'auth/email-already-exists') {
+            error_message.value = '入力したメールアドレスはすでに利用されています'
+        }
+        else if (error_code === 'auth/invalid-password') {
+            error_message.value = 'パスワードは6文字以上にしてください'
+        }
+        else 
+        {
+            error_message.value = '登録に失敗しました'
+        }
+    })
 }
 </script>
 
 <template>
     <div>
-        <input type="text" v-model="user_email">
+        <input type="email" v-model="user_email">
         <input type="text" v-model="user_password">
         <input type="text" v-model="user_repassword">
+        <p>{{error_message}}</p>
         <button @click="userSignUp(user_email, user_password)">登録</button>
         <button @click="googleSignIn">google登録</button>
     </div>
