9b47d702e5f60cb5a3a8fff5e74c14d6bec77f4d
guchio33 <aokihikari3@gmail.com>
2022-10-17 21:55:25 +0900

    カレンダーの追加

--- youlink/src/views/Post.vue
+++ youlink/src/views/Post.vue
@@ -8,13 +8,28 @@
       タグ<input type="text" v-model="tag"><br/>
     </div>
     <div>
-      日程<input type="text" v-model="date"><br/>
+      <Datepicker v-model="date1" range />
+      <div v-if=date1>
+        <Datepicker v-model="date2" range />
+      </div>
+      <div v-if=date2>
+        <Datepicker v-model="date3" range />
+      </div>
+      <div v-if=date3>
+        <Datepicker v-model="date4" range />
+      </div>
+      <div v-if=date4>
+        <Datepicker v-model="date5" range />
+      </div>
     </div>
     <div>
       時刻<input type="text" v-model="time"><br/>
     </div>
     <div>
       画像<input type="text" v-model="image"><br/>
+    </div>
+    <div>
+      一言<input type="text" v-model="describe"><br/>
     </div>
     <div>
       <button @click="create">登録</button>
@@ -24,7 +39,7 @@
 </template>
 
 <script lang="ts">
-import {defineComponent,ref} from 'vue'
+import {defineComponent,ref,onMounted} from 'vue'
 import { getFirestore,addDoc,collection,serverTimestamp, getDocs } from 'firebase/firestore'
 import {db}from '../FirebaseConfig'
 
@@ -33,20 +48,34 @@ export default defineComponent({
 
     const title = ref<string>()
     const tag = ref<string>()
-    const date = ref<string>()
+    const date1 = ref<any>()
+    const date2 = ref<any>()
+    const date3 = ref<any>()
+    const date4 = ref<any>()
+    const date5 = ref<any>()
     const time = ref<string>()
     const image = ref<string>()
+    const describe = ref<string>()
 
+    onMounted(() => {
+            const startDate = new Date();
+            const endDate = new Date(new Date().setDate(startDate.getDate() + 7));
+            date1.value = [startDate, endDate];
+    })
+
+
+  
     const create=()=>{
-      if(!title.value || tag.value || date.value || time.value || image.value)
+      if(!title.value || tag.value || date1.value || time.value || image.value||describe.value)
       try {
         console.log("起動");
         const  docRef = addDoc(collection(db, "users"), {
           title: title.value,
           tag: tag.value,
-          date: date.value,
+          date1: date1.value,
           time: time.value,
-          image: image.value
+          image: image.value,
+          describe: describe.value
         });
         console.log("Document written with ID: ", docRef);
       } catch (e) {
@@ -57,9 +86,14 @@ export default defineComponent({
     create,
     title,
     tag,
-    date,
+    date1,
+    date2,
+    date3,
+    date4,
+    date5,
     time,
-    image
+    image,
+    describe
   }
   }
 })
