c8d452628eaf2fbd9b5dce2af0bd53c2f8caa243
Rei-Suzuki1729 <eva2021end@gmail.com>
2022-10-18 22:46:29 +0900

    install

--- youlink/src/views/DirectMessage.vue
+++ youlink/src/views/DirectMessage.vue
@@ -1,10 +1,110 @@
 <template>
-  <div class="title">
-    <h1>DM画面</h1>
+  <div id="app">
+    <header class="header">
+      <h1>Chat</h1>
+      <!-- ログイン時にはフォームとログアウトボタンを表示 -->
+      <div v-if="user.uid" key="login">
+        [{{ user.displayName }}]
+        <button type="button" @click="doLogout">ログアウト</button>
+      </div>
+      <!-- 未ログイン時にはログインボタンを表示 -->
+      <div v-else key="logout">
+        <button type="button" @click="doLogin">ログイン</button>
+      </div>
+    </header>
+
+    <!--　Firebase から取得したリストを描画（トランジション付き）　-->
+    <transition-group name="chat" tag="div" class="list content">
+      <section v-for="{ key, name, image, message } in chat" :key="key" class="item">
+        <div class="item-image"><img :src="image" width="40" height="40"></div>
+        <div class="item-detail">
+          <div class="item-name">{{ name }}</div>
+          <div class="item-message">
+            <nl2br tag="div" :text="message"/>
+          </div>
+        </div>
+      </section>
+    </transition-group>
+  
+    <!-- 入力フォーム -->
+    <form action="" @submit.prevent="doSend" class="form">
+      <textarea
+        v-model="input"
+        :disabled="!user.uid"
+        @keydown.enter.exact.prevent="doSend"></textarea>
+      <button type="submit" :disabled="!user.uid" class="send-button">Send</button>
+    </form>
   </div>
 </template>
-<style>
-.title{
-    text-align: center;
+
+<script lang="ts">
+// firebase モジュール
+import firebase from 'firebase/firestore' 
+// 改行を <br> タグに変換するモジュール
+import Nl2br from 'vue-nl2br'
+export default {
+  components: { Nl2br },
+  data() {
+    return {
+      user: {},  // ユーザー情報
+      chat: [],  // 取得したメッセージを入れる配列
+      input: ''  // 入力したメッセージ
+    }
+  },
+  created() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.user = user ? user : {}
+      const ref_message = firebase.database().ref('message')
+      if (user) {
+        this.chat = []
+        // message に変更があったときのハンドラを登録
+        ref_message.limitToLast(10).on('child_added', this.childAdded)
+      } else {
+        // message に変更があったときのハンドラを解除
+        ref_message.limitToLast(10).off('child_added', this.childAdded)
+      }
+    })
+  },
+  methods: {
+    // ログイン処理
+    doLogin() {
+      const provider = new firebase.auth.TwitterAuthProvider()
+      firebase.auth().signInWithPopup(provider)
+    },
+    // ログアウト処理
+    doLogout() {
+      firebase.auth().signOut()
+    },
+    // スクロール位置を一番下に移動
+    scrollBottom() {
+      this.$nextTick(() => {
+        window.scrollTo(0, document.body.clientHeight)
+      })
+    },
+    // 受け取ったメッセージをchatに追加
+    // データベースに新しい要素が追加されると随時呼び出される
+    childAdded(snap: { val: () => any; key: any }) {
+      const message = snap.val()
+      this.chat.push({
+        key: snap.key,
+        name: message.name,
+        image: message.image,
+        message: message.message
+      })
+      this.scrollBottom()
+    },
+    doSend() {
+      if (this.user.uid && this.input.length) {
+        // firebase にメッセージを追加
+        firebase.database().ref('message').push({
+          message: this.input,
+          name: this.user.displayName,
+          image: this.user.photoURL
+        }, () => {
+          this.input = '' // フォームを空にする
+        })
+      }
+    }
+  }
 }
-</style>
+</script>
