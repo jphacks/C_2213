bd94185ad7c82daa2aaa17ef55b047a85d5dc799
sayyyyyy <d958956a6b650@gmail.com>
2022-10-20 21:03:48 +0900

    ホーム画面のデザインある程度完成

--- youlink/src/App.vue
+++ youlink/src/App.vue
@@ -9,5 +9,43 @@ import {  RouterView } from 'vue-router'
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+$mq-breakpoints: (
+    xl: 1440px,
+    lg: 1280px,
+    md: 1024px,
+    tb: 768px,
+    sm: 576px,
+    xs: 414px) !default;
+
+// Media Queryの設定
+@mixin media($breakpoint: md, $rule: min, $subtraction: false) {
+    $breakpoint: map_get($mq-breakpoints, $breakpoint);
+
+    @if $rule==max and $subtraction {
+        $breakpoint: $breakpoint - 0.0625;
+    }
+
+    @media screen and (#{$rule}-width: $breakpoint) {
+        @content;
+    }
+}
+
+body {
+  @include media(sm) {
+    font-size: 10px;
+  }
+
+  @include media(md) {
+    font-size: 12px;
+  }
+
+  @include media(lg) {
+    font-size: 14px;
+  }
+
+  @include media(xl) {
+    font-size: 16px;
+  }
+}
 </style>>
