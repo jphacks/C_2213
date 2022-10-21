8824fcdb78242007e991e23bbeaa60f863dc9702
Rei-Suzuki1729 <eva2021end@gmail.com>
2022-10-21 12:37:58 +0900

    linkをなくしました

--- youlink/src/App.vue
+++ youlink/src/App.vue
@@ -29,7 +29,6 @@ onAuthStateChanged(auth, (user) => {
         <RouterLink to="/notification">Notification</RouterLink><br>
         <RouterLink to="/post">Post</RouterLink><br>
         <RouterLink to="/users">Users</RouterLink><br>
-        <RouterLink to="/directmessage">DM</RouterLink><br>
       </div>
       <div class="scene">
         <RouterView /> 
