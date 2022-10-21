a8b555d6f04aa31bc24a911df8d7a7a1dc1e0539
Rei-Suzuki1729 <eva2021end@gmail.com>
2022-10-22 00:25:23 +0900

    DM機能の作成 #5

--- youlink/src/views/Offers.vue
+++ youlink/src/views/Offers.vue
@@ -16,25 +16,67 @@
       <div class="offer-inputarea-right">
         <p>ひとこと</p>
         <hr>
-        <textarea name="" v-model="message"></textarea>
+        <v-text-field
+            color="#2FDCE7"
+            variant="outlined"
+            hide-details
+            append-icon="mdi-send"
+            @click:append="apply"
+            v-model="message"
+        >
+        </v-text-field>
       </div>
     </div>
-    <button>応募</button>
+    <v-btn variant="outlined" @click="apply" >応募</v-btn>
+
 
   </div>
 </template>
 
-<script lang="ts">
-import {ref} from 'vue'
-export default ({
-  setup() {
-    const message=ref('')
+<script setup lang="ts">
+import { ref } from 'vue'
+import { addDoc,setDoc, collection, doc, serverTimestamp } from "@firebase/firestore";
+import { auth,db } from "../FirebaseConfig";
+import { useRouter } from 'vue-router'
 
-    return { message };
-  },
-});
+const user = auth.currentUser
+const router = useRouter()
 
+const message = ref('')
+
+const apply = async () => {
+  console.log("応募しました");
+  try {
+    const newDoc = doc(collection(db, "rooms")).id;
+    await setDoc(doc(db, "rooms", newDoc), {
+      room_id: newDoc,
+      title: "",
+      // owner_id: user.uid,
+      created_at: serverTimestamp(),
+      zoom_url: "",
+    });
+    await addDoc(collection(db, "rooms", newDoc, "chats"), {
+      text: message.value,
+      displayName: user.displayName,
+      created_at: serverTimestamp(),
+      uid: user.uid,
+    });
+    await addDoc(collection(db, "rooms", newDoc, "users"), {
+      uid: user.uid,
+      displayName: user.displayName,
+    });
+    await setDoc(doc(db, "users", user.uid, "room", newDoc), {
+      room_id: newDoc,
+      title: "",
+      Exist_room: true,
+    });
+    router.push({ name: 'directmessage', params: { id:newDoc} });
+    } catch (error) {
+        console.log(error);
+    }
+}
 </script>
+
 <style>
 .title{
     text-align: center;
