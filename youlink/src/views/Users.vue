5ee9e85c38679d222a31f1a1022e97b6fb57519b
home <ec98721zzz@gmail.com>
2022-10-22 07:52:56 +0900

    ユーザー認証情報取得

--- youlink/src/views/Users.vue
+++ youlink/src/views/Users.vue
@@ -1,10 +1,31 @@
 <template>
-  <div class="title">
-    <h1>ユーザー画面</h1>
-  </div>
+	<div class="title">
+		<h1>ユーザー画面</h1>
+		<div class="UserScene">
+			
+		</div>
+	</div>
 </template>
+
+<script>
+import { ref, onMounted } from 'vue'
+import { getUser } from '../stores/auth'
+
+export default ({
+	name: 'UserScene',
+	setup() {
+		
+		const user = getUser()
+		console.log(user)
+		
+	}
+});
+
+</script>
+
+
 <style>
 .title{
-    text-align: center;
+	text-align: center;
 }
 </style>
