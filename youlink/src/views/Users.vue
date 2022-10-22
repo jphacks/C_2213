4aea74335cda120b62d51d4ee9803462fd2638e6
home <ec98721zzz@gmail.com>
2022-10-22 09:29:58 +0900

    作業進捗10220930

--- youlink/src/views/Users.vue
+++ youlink/src/views/Users.vue
@@ -1,26 +1,72 @@
 <template>
-	<div class="title">
-		<h1>ユーザー画面</h1>
-		<div class="UserScene">
-			
-		</div>
-	</div>
+    <div>
+        <!-- header -->
+        <div class="title">
+            <h1>ユーザー画面</h1>
+        </div>
+        
+        <!-- body -->
+        <div v-if="user !== undefined">
+            <div>
+                <div v-if="user"
+                v-for="item in userInfo"
+	            >
+                	id:{{item.id}},name:{{item.name}}
+		            <br>
+		            <br>
+	            </div>
+	            
+                <div v-else>
+                    <h1>
+                    セッションタイムアウト  
+                    </h1>
+                </div>
+            </div>
+        </div>
+        
+        <!-- footer -->
+        <div />
+        
+    </div>
 </template>
 
-<script>
-import { ref, onMounted } from 'vue'
-import { getUser } from '../stores/auth'
+<script setup >
+	import { ref, watch, watchEffect } from "vue";
+	import { auth, db } from "../FirebaseConfig";
+	import { collection, query, onSnapshot, where } from "firebase/firestore";
 
-export default ({
-	name: 'UserScene',
-	setup() {
-		
-		const user = getUser()
-		console.log(user)
-		
-	}
-});
+	const user = ref(auth.currentUser);
+	//console.log(user["_rawValue"]["uid"])
+	const uid = user["_rawValue"]["uid"];
+	
+	const userInfo = ref([]);
+	const chatsRef = ref(null);
+	
+	
+	const q = query(collection(db, "users"), where("id", "==", `${uid}`));
+	const unsubscribe = onSnapshot(q, (snapshot) => {
+	    snapshot.docChanges().forEach((change) => {
+	        if (change.type === "added") {
+	            
+	            //console.log("Message: ", change.doc.id, change.doc.data());
+	            
+	            userInfo.value.push({
+	                id: change.doc.id,
+	                ...change.doc.data(),
+	            });
+	            
+	            console.log("userInfo", userInfo);
 
+	            setTimeout(() => {
+	                if (chatsRef.value !== null) {
+	                    console.log(chatsRef.value.scrollHeight);
+	                    window.scrollTo(0, chatsRef.value.scrollHeight);
+	                }
+	            }, 60);
+	        }
+	    });
+	});
+	
 </script>
 
 
